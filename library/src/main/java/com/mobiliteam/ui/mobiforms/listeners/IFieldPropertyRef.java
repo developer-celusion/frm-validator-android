22d6e139130a103091c9d95122acd2ec21d6feea
Swapnil N <swapnil.nandgave@celusion.com>
2018-05-05 11:09:58 +0530

    Added Interface for Property Ref

---
+++ library/src/main/java/com/mobiliteam/ui/mobiforms/listeners/IFieldPropertyRef.java
@@ -0,0 +1,11 @@
+package com.mobiliteam.ui.mobiforms.listeners;
+
+/**
+ * Created by swapnilnandgave on 04/05/18.
+ */
+
+public interface IFieldPropertyRef {
+
+    Object getFieldPropertyRefValue();
+
+}
